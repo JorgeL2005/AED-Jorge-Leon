424a55bc8c5b9e320ddd4ee58173940a79968088
JorgeL2005 <jorge.leon.v@utec.edu.pe>
2024-09-10 07:15:43 -0500

    DLL completo

--- DoublyLinkedList/DoublyLinkedList.cpp
+++ DoublyLinkedList/DoublyLinkedList.cpp
@@ -2,271 +2,314 @@
 
 using namespace std;
 
-// Estructura del Nodo
-template<typename T>
+template <typename T>
 struct Node {
     T data;
-    Node *next;
-    Node *prev;
-
+    Node* next;
+    Node* prev;
 };
 
-// Clase de la Lista
-template<typename T>
-class List {
+template <typename T>
+class DoublyLinkedList {
 private:
-    Node<T> *tail;  // cola
-    Node<T> *head;  // cabeza
+    Node<T>* head;
+    Node<T>* tail;
+
+    Node<T>* merge(Node<T>* first, Node<T>* second) {
+        if (!first)
+            return second;
+        if (!second)
+            return first;
+        if (first->data < second->data) {
+            first->next = merge(first->next, second);
+            if (first->next)
+                first->next->prev = first;
+            return first;
+        } else {
+            second->next = merge(first, second->next);
+            if (second->next)
+                second->next->prev = second;
+            return second;
+        }
+    }
+
+    Node<T>* mergeSort(Node<T>* node) {
+        if (!node || !node->next)
+            return node;
+        Node<T>* secondHalf = split(node);
+        return merge(mergeSort(node), mergeSort(secondHalf));
+    }
+
+    Node<T>* split(Node<T>* head) {
+        Node<T>* fast = head->next;
+        Node<T>* slow = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        Node<T>* temp = slow->next;
+        slow->next = nullptr;
+        if (temp)
+            temp->prev = nullptr;
+        return temp;
+    }
+
 public:
-    // Constructor
-    List() : head(nullptr), tail(nullptr) {}
-
-    //Retorna el elemento al comienzo
-    T front() {
-        return head->data;
+    DoublyLinkedList(){
+        head = nullptr;
+        tail = nullptr;
     }
-
-    //Retorna el elemento del final
-    T back() {
-        return tail->data;
+    ~DoublyLinkedList() {
+        clear();
     }
-
-    // Método para agregar un elemento al inicio de la lista
-    void push_front(T value) {
-        Node<T> *newNode = new Node<T>();       // Crear nuevo nodo
-        newNode->data = value;                  // Asignar valor al nodo
-        if (head == nullptr && tail == nullptr) {
-            head = newNode;                     // el nuevo nodo pasa a ser el (heap)=
-        } else {
-            newNode->next = head;               // El nuevo nodo apuntara a (head)
-            head->prev = newNode;               // El (head) apuntara al nuevo nodo
-            head = newNode;                     // el nuevo nodo pasa a ser el (heap)
+    T front(){
+        if(head != nullptr){
+            return head->data;
+        }
+        return T();
+    }
+    T back(){
+        if(tail != nullptr){
+            return tail->data;
+        }
+        return T();
+    }
+    void push_front(T data) {
+        Node<T>* newNode = new Node<T>();
+        newNode->data = data;
+        newNode->next = head;
+        newNode->prev = nullptr;
+        if (head != nullptr) {
+            head->prev = newNode;
+        }
+        head = newNode;
+        if (tail == nullptr) {
+            tail = head;
         }
     }
 
-    // Método para agregar un elemento al final de la lista
-    void push_back(T value) {
-        Node<T> *newNode = new Node<T>();
-        newNode->data = value;
-        if (head == nullptr && tail == nullptr) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            newNode->prev = tail;
+    void push_back(T data) {
+        Node<T>* newNode = new Node<T>();
+        newNode->data = data;
+        newNode->next = nullptr;
+        newNode->prev = tail;
+        if (tail != nullptr) {
             tail->next = newNode;
-            tail = newNode;
+        }
+        tail = newNode;
+        if (head == nullptr) {
+            head = tail;
         }
     }
 
-    // Remueve el elemento al comienzo
     T pop_front() {
-        if (head == tail){
-            head = nullptr;
-            tail = nullptr;
-        }
-        else {
-            Node<T> *temp = head;
-            head = temp->next;
-            head->prev = nullptr;
+        if (head != nullptr) {
+            Node<T>* temp = head;
+            head = head->next;
+            if (head) {
+                head->prev = nullptr;
+            } else {
+                tail = nullptr;
+            }
+            T data = temp->data;
             delete temp;
+            return data;
         }
+        return T();
     }
 
-    // Remueve el elemento al final
     T pop_back() {
-        if(head ==tail){
-            head = nullptr;
-            tail = nullptr;
-        }
-        else {
-            Node<T> *temp = tail;
-            tail = temp->prev;
-            tail->next = nullptr;
+        if (tail != nullptr) {
+            Node<T>* temp = tail;
+            tail = tail->prev;
+            if (tail) {
+                tail->next = nullptr;
+            } else {
+                head = nullptr;
+            }
+            T data = temp->data;
             delete temp;
+            return data;
         }
+        return T();
     }
 
-    // Inserta en cualquier posición
-    void insert(T value, int position) {
-        Node<T> *newNode = new Node<T>();
-        newNode->data = value;
-        Node<T> *temp = head;
-        if (position == 0) {
-            push_front(value);
+    T operator[](int index) {
+        Node<T>* temp = head;
+        for (int i = 0; i < index && temp != nullptr; i++) {
+            temp = temp->next;
+        }
+        if (temp != nullptr) {
+            return temp->data;
+        }
+        return T();
+    }
+
+    void insert(T data, int index) {
+        if (index == 0) {
+            push_front(data);
         } else {
-            for (int i = 0; i < position - 1; i++) {
+            Node<T>* temp = head;
+            for(int i = 0; i < index - 1; i++){
+                if(temp->next == nullptr){
+                    return;
+                }
                 temp = temp->next;
             }
-            if (temp == tail) {
-                push_back(value);
-            } else {
+            Node<T>* newNode = new Node<T>();
+            newNode->data = data;
+            newNode->next = temp->next;
+            newNode->prev = temp;
+            if (temp->next != nullptr) {
                 temp->next->prev = newNode;
-                newNode->prev = temp;
-                newNode->next = temp->next;
-                temp->next = newNode;
             }
-
+            temp->next = newNode;
+            if (tail == temp) {
+                tail = newNode;
+            }
         }
     }
 
-    // Remueve en cualquier posicion
-    void remove(int position) {
-
-        Node<T> *temp = head;
-        if (position == 0) {
+    void remove(int index){
+        if (index == 0) {
             pop_front();
         } else {
-            for (int i = 0; i < position ; i++) {
+            Node<T>* temp = head;
+            for(int i = 0; i < index - 1; i++){
+                if(temp->next == nullptr){
+                    return;
+                }
                 temp = temp->next;
             }
-            if (temp == tail) {
-                pop_back();
-            } else {
-                temp->next->prev = temp->prev;
-                temp->prev->next = temp->next;
-                delete temp;
+            Node<T>* toDelete = temp->next;
+            temp->next = toDelete->next;
+            if (toDelete->next != nullptr) {
+                toDelete->next->prev = temp;
             }
-
+            if (tail == toDelete) {
+                tail = temp;
+            }
+            delete toDelete;
         }
     }
-    //Retorna el elemento en la posición indicada
-    T operator[ ](int position){
-        Node<T> *temp = head;
-        for (int i = 0; i < position ; i++) {
-            temp = temp->next;
-        }
-        return temp->data;
-    }
 
-    //Retorna si la lista está vacía o no
     bool empty(){
-        if(head == nullptr && tail == nullptr){
-            return true;
-        }
-        else {
-            return false;
-        }
+        return head == nullptr;
     }
 
-
-    // Retorna el tamaño de la lista
     int size(){
+        int count = 0;
         Node<T>* temp = head;
-        int i = 0;
-        if (head == nullptr && tail == nullptr){
-            return 0;
-        }
-        else {
-            while (temp != tail) {
-                temp = temp->next;
-                i++;
-            }
-            return i;
-        }
-    }
-    // Elimina todos los elementos de la lista
-    void clear(){
-        while (head != nullptr) {
-            remove(0);
-        }
-    }
-
-
-    // Revierte la lista
-    void reverse() {
-        Node<T>* temp = head;
-        Node<T>* temp1 = nullptr;
-        while (temp != nullptr) {
-            temp1 = temp->prev;
-            temp->prev = temp->next;
-            temp->next = temp1;
-            temp = temp->prev;
-        }
-        if (temp1 != nullptr) {
-            temp1 = head;
-            head = tail;
-            tail = temp1;
-        }
-    }
-
-
-    // Método para imprimir todos los elementos de la lista
-    void print() const {
-        Node<T> *temp = head;
-        while (temp != nullptr) {
-            std::cout << temp->data << " ";
+        while(temp != nullptr){
             temp = temp->next;
+            count++;
         }
-        std::cout << std::endl;
+        return count;
     }
 
-    // Destructor para liberar memoria
-    ~List() {
+    void clear() {
         while (head != nullptr) {
-            Node<T> *temp = head;
+            Node<T>* temp = head;
             head = head->next;
             delete temp;
         }
+        tail = nullptr;
+    }
+
+    void sort() {
+        // ordenar
+        head = mergeSort(head);
+        // actualizar tail
+        Node<T>* temp = head;
+        while (temp && temp->next) {
+            temp = temp->next;
+        }
+        tail = temp;
+    }
+
+    void reverse() {
+        if (head != nullptr) {
+            Node<T>* current = head;
+            Node<T>* temp = nullptr;
+            while (current != nullptr) {
+                temp = current->prev;
+                current->prev = current->next;
+                current->next = temp;
+                current = current->prev;
+            }
+            if (temp != nullptr) {
+                head = temp->prev;
+            }
+        }
     }
 };
 
 
-int main() {
-    List<int> myList;
-    cout << "Foward List: " << endl;
-    myList.push_back(1);
-    myList.push_back(2);
-    myList.push_back(3);
-    myList.push_back(4);
-    myList.print();
 
-    myList.pop_front();
-    cout << "Pop_front: " << endl;
-    myList.print();
-    cout << "Pop_back: " << endl;
-    myList.pop_back();
-    myList.print();
-    cout << "Insert(1,0): " << endl;
-    myList.insert(1, 0);
-    myList.print();
-    cout << "Insert(4,3): " << endl;
-    myList.insert(4, 3);
-    myList.print();
-    cout << "Remove(3): " << endl;
-    myList.remove(2);
-    myList.print();
-    cout << "Insert(4,3): " << endl;
-    myList.insert(3, 2);
-    myList.print();
-    // cout << "Retornar elemento de la posicion 3: \n" << myList.operator  << endl;
-    cout << "La lista es vacia? \n" << myList.empty() << endl;
-    myList.remove(0);
-    myList.remove(0);
-    myList.remove(0);
-    myList.remove(0);
-    myList.print();
 
-    cout << "La lista es vacia? \n" << myList.empty() << endl;
-    cout << "tamaño de la lista: \n" << myList.size() << endl;
-    myList.push_back(1);
-    myList.print();
 
-    List<int> myList1;
-    myList1.push_back(1);
-    myList1.push_back(2);
-    myList1.push_back(3);
-    myList1.print();
-    cout << myList1.back() << endl;
 
-    cout << "tamaño de la lista: \n" << myList.size() << endl;
+int main(){
+    // test lista vacia
+    DoublyLinkedList<double> list1;
+    cout << list1.front() << endl;
+    cout << list1.back() << endl;
+    cout << list1.pop_front() << endl;
+    cout << list1.pop_back() << endl;
+    cout << list1[3] << endl;
+    cout << list1.size() << endl;
+    // test push
+    DoublyLinkedList<string> list2;
+    list2.push_front("front1");
+    list2.push_front("front2");
+    list2.push_back("back1");
+    list2.push_back("back2");
+    cout << list2.front() << endl;
+    cout << list2.back() << endl;
+    // test []
+    cout << "Imprimiendo " <<list2.size() << " elementos:"<< endl;
+    for (int i = 0; i < list2.size(); i++){
+        cout <<"- "<< list2[i] << endl;
+    }
 
-    cout << "Foward List: " << endl;
-    myList.push_back(2);
-    myList.push_back(3);
-    myList.push_back(4);
-    myList.print();
-    myList.reverse();
-    myList.print();
+    // test pop
+    cout << list2.pop_front() << endl;
+    cout << list2.pop_back() << endl;
+    cout << list2.front() << endl;
 
-    myList.~List();
+    // test clear
+    list2.clear();
+    cout << list2.size() << endl;
+
+    // test reverse, insert, remove, sort
+    DoublyLinkedList<int> list3;
+    list3.push_front(5);
+    list3.push_front(2);
+    list3.push_front(4);
+    list3.push_front(8);
+    list3.insert(99,7);
+    list3.remove(7);
+    list3.push_front(1);
+    list3.insert(5,3);
+    list3.push_front(9);
+    list3.push_front(6);
+    list3.insert(6, 7);
+    cout << "original: ";
+    for (int i = 0; i < list3.size(); i++){
+        cout << list3[i] << " ";
+    }
+    cout << "\n";
+    list3.sort();
+    cout << "sorted: ";
+    for (int i = 0; i < list3.size(); i++){
+        cout << list3[i] << " ";
+    }
+    cout << "\n";
+    list3.reverse();
+    cout << "reversed: ";
+    for (int i = 0; i < list3.size(); i++){
+        cout << list3[i] << " ";
+    }
+
+    return 0;
 }
