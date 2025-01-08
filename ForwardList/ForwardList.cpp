8999b321941be4f11150f3d27f73efd6ebb34920
JorgeL2005 <jorge.leon.v@utec.edu.pe>
2025-01-07 20:58:33 -0500

    Forwardlist - edicion verano sin ti

--- ForwardList/ForwardList.cpp
+++ ForwardList/ForwardList.cpp
@@ -1,156 +1,189 @@
 #include <iostream>
 
-// Listas enlazadas (Forward List)
-
-
 using namespace std;
+
 template <typename T>
-struct Node{
+struct Node {
     T data;
-    Node<T>* next;
+    Node* next;
 };
 
-template<typename T>
-class List{
+template <typename T>
+class List {
 private:
     Node<T>* head;
+
+    Node<T>* merge(Node<T>* first, Node<T>* second) {
+        if (!first)
+            return second;
+        if (!second)
+            return first;
+        if (first->data < second->data) {
+            first->next = merge(first->next,second);
+            return first;
+        } else {
+            second->next = merge(first,second->next);
+            return second;
+        }
+    }
+    Node<T>* mergeSort(Node<T>* head) {
+        if (!head || !head->next)
+            return head;
+        Node<T>* slow = head;
+        Node<T>* fast = head->next;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        Node<T>* temp = slow->next;
+        slow->next = nullptr;
+        return merge(mergeSort(head), mergeSort(temp));
+    }
+
 public:
     List(){
         head = nullptr;
-    };
-
-    List(T dato){
-        head->data = dato;
-        head->next = nullptr;
     }
-
-    ~List(){
+    ~List() {
         clear();
     }
-
     T front(){
-        if(head == nullptr){
-            return T();
-        }
-        else{
+        if(head != nullptr){
             return head->data;
         }
-    }; // Retorna el elemento al comienzo
-
+        return T();
+    }
     T back(){
-        if (head == nullptr){
-            return T();
+        if(head != nullptr){
+            Node<T>* temp = head;
+            while(temp->next != nullptr){
+                temp = temp->next;
+            }
+            return temp->data;
         }
-        Node<T>* temp = head;
-        while(temp->next != nullptr){
-            temp = temp->next;
-        }
-        return temp->data;
-
-    }; // Retorna el elemento al final
-
-    void push_front(T dato){
-        Node<T>* nuevo = new Node<T>;
-        nuevo->data = dato;
-        nuevo->next = head;
-        head = nuevo;
-    }; // Agrega un elemento al comienzo
-
-    void push_back(T dato){
-        Node<T>* nuevo = new Node<T>;
-        nuevo->data = dato;
-        Node<T>* temp = head;
-        while (temp->next != nullptr){
-            temp = temp -> next;
-        }
-        temp->next = nuevo;
-        nuevo->next = nullptr;
-    }; // Agrega un elemento al final
-
-    T pop_front(){
-        if (head == nullptr){
-            return T();
+        return T();
+    }
+    void push_front(T data){
+        Node<T>* newNode = new Node<T>();
+        newNode->data = data;
+        newNode->next = head;
+        head = newNode;
+    }
+    void push_back(T data){
+        Node<T>* newNode = new Node<T>();
+        newNode->data = data;
+        newNode->next = nullptr;
+        if(!head){
+            head = newNode;
         }else{
             Node<T>* temp = head;
-            T data = temp->data;
+            while(temp->next != nullptr){
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+    T pop_front(){
+        if(head){
+            Node<T>* temp = head;
             head = head->next;
+            T data = head->data;
             delete temp;
             return data;
         }
-    }; // Remueve el elemento al comienzo
-
+        return T();
+    }
     T pop_back(){
-        if (head == nullptr){
-            return T();
-        }else{
-            if (head->next == nullptr){
-                T data = head->data;
-                delete head;
-                head = nullptr;
-                return data;
-            }
-            else{
-                Node<T>* temp = head;
-                while(temp->next->next != nullptr){
-                    temp = temp -> next;
-                }
-                T data = temp->next->data;
-                delete temp -> next;
-                temp->next = nullptr;
-                return data;
-            }
-        }
-    }; // Remueve el elemento al final
-
-    T operator[ ](int pos){
-        if (head == nullptr){
-            return T();
-        }
-        Node<T>* temp = head;
-        for(int i = 0; i < pos; i++){
-            if(temp->next == nullptr){
-                return T();
-            }
-            temp = temp->next;
-        }
-        return temp->data;
-    }; // Retorna el elemento en la posición indicada
-
-    bool empty(){
-        if (head == nullptr){
-            return true;
-        }else
-            return false;
-    }; // Retorna si la lista está vacía o no
-
-    int size(){
-        Node<T>* temp = head;
-        int contador = 0;
-        if (head == nullptr){
-            return 0;
-        }
-        if (head->next == nullptr){
-            return 1;
-        }
-        while (temp != nullptr){
-            temp = temp->next;
-            contador += 1;
-        }
-        return contador;
-    }; // Retorna el tamaño de la lista
-
-    void clear(){
-        while (head != nullptr){
+        if(head){
             Node<T>* temp = head;
-            head = head -> next;
+            Node<T>* prev = nullptr;
+            while(temp->next != nullptr){
+                prev = temp;
+                temp = temp->next;
+            }
+            if(prev){
+                prev->next = nullptr;
+            }else{
+                head = nullptr;
+            }
+            T data = temp->data;
+            delete temp;
+            return data;
+        }
+        return T();
+    }
+    T operator[](int index){
+        if(head != nullptr){
+            Node<T>* temp = head;
+            for(int i = 0; i < index; i++){
+                if(temp->next == nullptr){
+                    return T();
+                }
+                temp = temp->next;
+            }
+            return temp->data;
+        }
+        return T();
+    }
+    bool isEmpty(){
+        return head == nullptr;
+    }
+    void insert(int index, T data){
+        if(index == 0){
+            push_front(data);
+        }else{
+            Node<T>* temp = head;
+            for(int i = 0; i < index - 1; i++){
+                if(temp->next == nullptr){
+                    return;
+                }
+                temp = temp->next;
+            }
+            Node<T>* newNode = new Node<T>();
+            newNode->data = data;
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+    }
+    void remove(int index){
+        if(head != nullptr){
+            if(index == 0){
+                pop_front();
+            }else{
+                Node<T>* temp = head;
+                for(int i = 0; i < index - 1; i++){
+                    if(temp->next == nullptr){
+                        return;
+                    }
+                    temp = temp->next;
+                }
+                if(temp->next != nullptr){
+                    Node<T>* toDelete = temp->next;
+                    temp->next = toDelete->next;
+                    delete toDelete;
+                }
+            }
+        }
+    }
+    int size(){
+        int count = 0;
+        Node<T>* temp = head;
+        while(temp != nullptr){
+            temp = temp->next;
+            count++;
+        }
+        return count;
+    }
+    void clear(){
+        while(head != nullptr){
+            Node<T>* temp = head;
+            head = head->next;
             delete temp;
         }
-    }; // Elimina todos los elementos de la lista
-
-    void sort(){
-
-    }; // Implemente un algoritmo de ordenacion con listas enlazadas)
-
+    }
+    void sort() {
+        head = mergeSort(head);
+    }
     void reverse(){
         if(head != nullptr){
             Node<T>* prev = nullptr;
@@ -165,68 +198,92 @@ public:
             head = prev;
         }
     }
+    Node<T> *getHead(){
+        return head;
+    }
+    friend ostream& operator<<(ostream& os, List<T>& list){
+        Node<T>* temp = list.head;
+        os << "[";
+        while(temp != nullptr){
+            os << temp->data;
+            if (temp->next != nullptr){
+                os << ", ";
+            }
+            temp = temp->next;
+        }
+        os << "]";
+        return os;
+    }
+};
 
-    }; // Revierte la lista
+
+
+
 
 
 int main(){
-// Pruebas de los códigos
+    // test lista vacia
+    cout << "Test lista vacia" << endl;
+    List<double> list1;
+    cout << list1.front() << endl;
+    cout << list1.back() << endl;
+    cout << list1.pop_front() << endl;
+    cout << list1.pop_back() << endl;
+    cout << list1[3] << endl;
+    cout << list1.size() << endl;
+    // test push
+    cout << "Test push" << endl;
+    List<string> list2;
+    list2.push_front("front1");
+    list2.push_front("front2");
+    list2.push_back("back1");
+    list2.push_back("back2");
+    cout << list2.front() << endl;
+    cout << list2.back() << endl;
+    // test []
+    cout << "Test []" << endl;
+    cout << "Imprimiendo " <<list2.size() << " elementos:"<< endl;
+    for (int i = 0; i < list2.size(); i++){
+        cout <<"- "<< list2[i] << endl;
+    }
 
-    // Test de vacio
-    cout << "Test en lista vacia: " << endl;
-    List<double> lista; // Creo una lista vacía
-    cout << lista.front() << endl;
-    cout << lista.back() << endl;
-    cout << lista.size() << endl;
+    // test pop
+    cout << "Test Pop" << endl;
+    cout << list2.pop_front() << endl;
+    cout << list2.pop_back() << endl;
+    cout << list2.front() << endl;
 
-    // Test de pushes
-    cout << "Test de pushear valores: " << endl;
-    List<float> lista2(8.8);
-    lista2.push_front(1.1);
-    lista2.push_front(2.1);
-    lista2.push_front(3.1);
-    lista2.push_back(4.1);
-    lista2.push_back(5.1);
-    cout << lista2.front() << endl;
-    cout << lista2.back() << endl;
+    // test clear
+    cout << "Test clear" << endl;
+    list2.clear();
+    cout << list2.size() << endl;
 
-    // test imprimir lista
-    cout << "Lista: "<< endl;
-    for (int i = 0; i < lista2.size(); i++){
-        cout << lista2[i] << " -> ";
-    } cout << "null";
-    cout << endl;
-
-    // Test de pops
-    cout << "Test de pops: " << endl;
-    lista2.pop_front();
-    lista2.pop_back();
-    cout << lista2.front() << endl;
-    cout << lista2.back() << endl;
-
-
-
-    // test reverse
+    // test reverse, insert, remove, sort
+    cout << "Test reverse. insert, remove, sort" << endl;
     List<int> list3;
     list3.push_front(5);
     list3.push_front(2);
     list3.push_front(4);
     list3.push_front(8);
-    list3.push_front(7);
+    list3.insert(99,7);
+    list3.remove(7);
     list3.push_front(1);
-    list3.push_front(3);
+    list3.insert(5,3);
     list3.push_front(9);
     list3.push_front(6);
-    cout << "Lista original: ";
-    for (int i = 0; i < list3.size(); i++){
-        cout << list3[i] << " -> ";
-    } cout << "null";
+    list3.insert(6, 7);
 
+    cout << "original: ";
+    cout << list3 << endl;
+
+    list3.sort();
+    cout << "sorted: ";
+    cout << list3 << endl;
+
+    cout << "\n";
     list3.reverse();
-    cout << endl;
-    cout << "reverso: ";
-    for (int i = 0; i < list3.size(); i++){
-        cout << list3[i] << " -> ";
-    } cout << "null";
+    cout << "reversed: ";
+    cout << list3 << endl;
 
+    return 0;
 }
