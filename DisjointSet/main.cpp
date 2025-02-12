26b1488c456bd3cedf183ee4744249d4f19b1833
JorgeL2005 <jorge.leon.v@utec.edu.pe>
2025-02-12 00:35:34 -0500

    DisjointSet

---
+++ DisjointSet/main.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+
+using namespace std;
+// Implementación de Disjoint Set (Union-Find)
+class DisjointSet {
+private:
+    vector<int> parent;
+    vector<int> rank;
+
+public:
+    DisjointSet(int n) {
+        parent.resize(n);
+        rank.resize(n, 1);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int u) {
+        if (parent[u] != u) {
+            parent[u] = find(parent[u]);
+        }
+        return parent[u];
+    }
+
+    void union_sets(int u, int v) {
+        int rootU = find(u);
+        int rootV = find(v);
+        if (rootU != rootV) {
+            if (rank[rootU] > rank[rootV]) {
+                parent[rootV] = rootU;
+            } else if (rank[rootU] < rank[rootV]) {
+                parent[rootU] = rootV;
+            } else {
+                parent[rootV] = rootU;
+                rank[rootU]++;
+            }
+        }
+    }
+};
