742bf2fa4e178543219a8d1b8409d13a81fade22
JorgeL2005 <jorge.leon.v@utec.edu.pe>
2025-02-22 19:04:25 -0500

    Quadtree version2

--- ProyectoQT/main.cpp
+++ ProyectoQT/main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -8,21 +7,15 @@
 #include "stb_image.h"
 #include "stb_image_write.h"
 
-
-
-
 using namespace std;
 
-// Parámetros de compresión
 #define MAX_DEPTH 8
-#define THRESHOLD 13  // Nivel de detalle permitido en un bloque
+#define THRESHOLD 15
 
-// Estructura para un píxel RGB
 struct Pixel {
     uint8_t r, g, b;
 };
 
-// Nodo del QuadTree
 struct QuadTreeNode {
     int x, y, size;
     Pixel avgColor;
@@ -35,17 +28,15 @@ struct QuadTreeNode {
     }
 };
 
-// Clase QuadTree
 class QuadTree {
 private:
     QuadTreeNode* root;
     vector<vector<Pixel>> image;
 
-    // Calcula el color promedio de una región
     Pixel computeAverageColor(int x, int y, int size) {
         int sumR = 0, sumG = 0, sumB = 0, count = 0;
-        for (int i = y; i < y + size; i++) {
-            for (int j = x; j < x + size; j++) {
+        for (int i = y; i < y + size && i < image.size(); i++) {
+            for (int j = x; j < x + size && j < image[0].size(); j++) {
                 sumR += image[i][j].r;
                 sumG += image[i][j].g;
                 sumB += image[i][j].b;
@@ -55,25 +46,25 @@ private:
         return { (uint8_t)(sumR / count), (uint8_t)(sumG / count), (uint8_t)(sumB / count) };
     }
 
-    // Calcula la desviación estándar de una región
     double computeDetail(int x, int y, int size, Pixel avg) {
         double sumDiff = 0;
-        for (int i = y; i < y + size; i++) {
-            for (int j = x; j < x + size; j++) {
+        int count = 0;
+        for (int i = y; i < y + size && i < image.size(); i++) {
+            for (int j = x; j < x + size && j < image[0].size(); j++) {
                 sumDiff += pow(image[i][j].r - avg.r, 2);
                 sumDiff += pow(image[i][j].g - avg.g, 2);
                 sumDiff += pow(image[i][j].b - avg.b, 2);
+                count++;
             }
         }
-        return sqrt(sumDiff / (size * size * 3));
+        return sqrt(sumDiff / (count * 3));
     }
 
-    // Inserta y segmenta la imagen
     void insert(QuadTreeNode*& node, int x, int y, int size, int depth) {
-        if (!node) {
-            Pixel avg = computeAverageColor(x, y, size);
-            node = new QuadTreeNode(x, y, size, avg);
-        }
+        if (size <= 1 || x >= image[0].size() || y >= image.size()) return;
+
+        Pixel avg = computeAverageColor(x, y, size);
+        node = new QuadTreeNode(x, y, size, avg);
 
         if (depth >= MAX_DEPTH || computeDetail(x, y, size, node->avgColor) <= THRESHOLD) {
             node->isLeaf = true;
@@ -82,19 +73,18 @@ private:
 
         node->isLeaf = false;
         int half = size / 2;
+
         insert(node->children[0], x, y, half, depth + 1);
         insert(node->children[1], x + half, y, half, depth + 1);
         insert(node->children[2], x, y + half, half, depth + 1);
         insert(node->children[3], x + half, y + half, half, depth + 1);
     }
 
-    // Dibuja la imagen comprimida
     void drawSegmentation(QuadTreeNode* node, vector<vector<Pixel>>& output) {
         if (!node) return;
-
         if (node->isLeaf) {
-            for (int i = node->y; i < node->y + node->size; i++) {
-                for (int j = node->x; j < node->x + node->size; j++) {
+            for (int i = node->y; i < node->y + node->size && i < output.size(); i++) {
+                for (int j = node->x; j < node->x + node->size && j < output[0].size(); j++) {
                     output[i][j] = node->avgColor;
                 }
             }
@@ -106,13 +96,11 @@ private:
     }
 
 public:
-    // Constructor
     QuadTree(vector<vector<Pixel>>& img) : image(img) {
         root = nullptr;
-        insert(root, 0, 0, img.size(), 0);
+        insert(root, 0, 0, image[0].size(), 0);
     }
 
-    // Guarda la imagen comprimida
     void saveCompressedImage(const string& filename) {
         vector<vector<Pixel>> output = image;
         drawSegmentation(root, output);
@@ -134,7 +122,6 @@ public:
     }
 };
 
-// Cargar imagen con STB Image
 vector<vector<Pixel>> loadImage(const string& filename, int& width, int& height) {
     int channels;
     uint8_t* data = stbi_load(filename.c_str(), &width, &height, &channels, 3);
@@ -145,7 +132,6 @@ vector<vector<Pixel>> loadImage(const string& filename, int& width, int& height)
     }
 
     vector<vector<Pixel>> img(height, vector<Pixel>(width));
-
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             int idx = (i * width + j) * 3;
@@ -157,13 +143,12 @@ vector<vector<Pixel>> loadImage(const string& filename, int& width, int& height)
     return img;
 }
 
-// Main
 int main() {
     int width, height;
-    vector<vector<Pixel>> img = loadImage("man.jpg", width, height);
+    vector<vector<Pixel>> img = loadImage("images/goku.jpg", width, height);
 
     QuadTree qt(img);
-    qt.saveCompressedImage("compressed.jpg");
+    qt.saveCompressedImage("goku-compressed.jpg");
 
     cout << "Imagen comprimida guardada como compressed.jpg" << endl;
     return 0;
